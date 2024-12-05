0099d3ecd4e05f611e50608101568b3a43841854
Alfarell Muchamad Yuwanto <malfarell@gmail.com>
2024-12-05 19:20:16 +0700

    feat(footer): update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,11 @@ import { Layout } from 'antd';
 const { Footer } = Layout;
 
 const MainFooter = () => {
-  return <Footer className='mt-auto'>Footer</Footer>;
+  return (
+    <Footer className='mt-auto'>
+      <div className='container mx-auto text-center'>BlogApp</div>
+    </Footer>
+  );
 };
 
 export default MainFooter;
